cb36e7a0772c136189ba3a265934b99cfb00de18
lifepilot-jared <jared.hendriks6+lifepilot@gmail.com>
2026-07-14 04:28:21 +0930

    Switch standalone game to responsive V2.2 runner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { SuperZoosAdventureV2 } from "./v2/SuperZoosAdventureV2";
-import "./v2/v2Milestone.css";
+import { SuperZoosAdventureV22 } from "./v2/SuperZoosAdventureV22";
 
 export default function App() {
-  return <SuperZoosAdventureV2 />;
+  return <SuperZoosAdventureV22 />;
 }
